c6893f03ab380ffb6b1fde9d8cac43cffb1b6190
Frank Ma <ma990801@gmail.com>
2020-06-21 10:53:26 -0400

    Added function to find line start and end in a frame given slope and intercept

--- analyses/draw.py
+++ analyses/draw.py
@@ -6,7 +6,46 @@ import numpy as np
 Reference: https://github.com/nadamian/VCTrack/blob/master/draw.py
 """
 
-def draw(path, lines, angles, outfile, videotype='.mp4'):
+def find_line_start_end(m, b, w, h):
+    """
+    The goal here is to find points at the edges of the frame that can
+    serve as the start and end point of our line. This function takes in
+    the slope and y intercept of the line and the frame size and returns
+    the coordinate of thw two points of intersection.
+
+    Given a line, there are four possible points it can intersect with
+    a rectangle that is the size of our frame. They are:
+    - the y intercept: (0, b)
+    - the x intercept: (-b/m, 0)
+    - intersection between the line and x = w: (w, mw + b)
+    - intersection between the line and y = h: ((h - b)/m, h)
+
+    Only two of these points of intersections should be present at a time.
+    Meaning that only two of the following four conditions will be true at
+    one time.
+    - 0 <= b <= h
+    - 0 <= -b/m <= w
+    - 0 <= mw + b <= h
+    - 0 <= (h - b)/m <= w
+    """
+    intersect = []
+    if b >= 0 and b <= h:
+        intersect.append((0, b))
+    if (-b/m) >= 0 and (-b/m) <= w:
+        if (-b/m, 0) not in intersect:
+            intersect.append((-b/m, 0))
+    if (m*w+b) >= 0 and (m*w+b) <= h:
+        if (w, m*w + b) not in intersect:
+            intersect.append((w, m*w + b))
+    if (h-b)/m >= 0 and (h-b)/m <= w:
+        if ((h-b)/m, h) not in intersect:
+            intersect.append(((h-b)/m, h))
+    if len(intersect) is not 2:
+        raise RuntimeError("Line end points calculation error")
+    return intersect
+
+
+def draw(path, startframe, endframe, lines, angles, outfile, videotype='.mp4'):
     """Takes each frame from video and stitches it back into new video with
     line drawn on."""
     
@@ -16,10 +55,12 @@ def draw(path, lines, angles, outfile, videotype='.mp4'):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    # make sure that the first frame read will be the start frame
+    cap.set(cv2.CV_CAP_PROP_POS_FRAMES, startframe - 1)
     # s signals whether or not we are at the end, im is the frame
     s, im = cap.read() 
     # count for which frame we are on
-    count = 0
+    count = startframe
 
     # specify output video format and output path
     name = path[path.rfind('\\') + 1:path.rfind('.')] + 'with_lines'
@@ -30,11 +71,12 @@ def draw(path, lines, angles, outfile, videotype='.mp4'):
     else:
         fourcc = 0
 
-    # w is the output
+    # w is the video object to be outputted
     w = cv2.VideoWriter(os.path.join(outfile, name + videotype), \
                         fourcc, frames, (width, height))
 
-    while s:
+    # loop until we're at the endframe or at the end of the video
+    while s and (count is not endframe):
         # print progress to stdout
         if count % 5 == 0:
             print('Printing lines on your video.')
