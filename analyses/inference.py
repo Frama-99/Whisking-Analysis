af616de451b1cf00e5e2c8027961f30707096035
Frank Ma <ma990801@gmail.com>
2020-10-10 00:10:19 -0400

    Added argument parser to main and added inference.py to interface with DLC

--- analyses/inference.py
+++ analyses/inference.py
@@ -1,2 +1,8 @@
 import deeplabcut
+import yaml
 
+DLC_CFG_PATH = r'C:\Users\Ma990\OneDrive - Tufts\Jobs\SPEL\Whisking\Whisking_with_Midline-Frank Ma-2020-05-03\config.yaml'
+
+def do_dlc_inference(video, dest):
+    deeplabcut.analyze_videos(config_path=DLC_CFG_PATH, videos=[video], save_as_csv=False, destfolder=dest)
+    
--- analyses/main.py
+++ analyses/main.py
@@ -2,7 +2,33 @@ from analysis import Analysis
 from draw import draw
 
 def main():
-    h5_path = r'C:\Users\Ma990\OneDrive - Tufts\Jobs\SPEL\Whisking\Whisking_with_Midline-Frank Ma-2020-05-03\videos\iteration-2\6400ratDLC_resnet50_Whisking_with_MidlineMay3shuffle1_500000.h5'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v',
+                        '--videodir',
+                        help='Path to the video file or directory',
+                        dest='video_dir',
+                        required=False,
+                        metavar='VIDEODIR')
+    parser.add_argument('-h',
+                        '--h5dir',
+                        help='Path to the h5 file',
+                        dest='h5_dir',
+                        required=False,
+                        metavar='H5DIR')
+    parser.add_argument('output_dir',
+                        help='Output Directory of the json files',
+                        nargs='?',
+                        metavar='OUTDIR')
+    args = parser.parse_args()
+    
+    if parser.h5_dir is not None:
+        pass
+    elif parser.video_dir is not None:
+        pass
+    else:
+        raise RuntimeError("You need to provide either a video directory or the DLC generated .h5 file.")
+
+    # h5_path = r'C:\Users\Ma990\OneDrive - Tufts\Jobs\SPEL\Whisking\Whisking_with_Midline-Frank Ma-2020-05-03\videos\iteration-2\6400ratDLC_resnet50_Whisking_with_MidlineMay3shuffle1_500000.h5'
     DLCscorer = 'DLC_resnet50_Whisking_with_MidlineMay3shuffle1_500000'
     analysis = Analysis(h5_path, DLCscorer)
 
